d998751b4513d76d75edc6f51ebf3eeef968005c
Beringer Dorottya <beringerdorottya@gmail.com>
2023-12-06 17:50:48 +0100

    finished javascript, added icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,95 @@
 import React from 'react';
 import './App.css';
 
+function formatTime(millisec) {
+  let min = Math.floor(millisec / (60 * 1000));
+  if (min < 10) {
+    min = '0' + min.toString();
+  } else {
+    min = min.toString();
+  }
+  const remainingMillisec = millisec % (60 * 1000);
+  let sec = Math.floor(remainingMillisec / 1000);
+  if (sec < 10) {
+    sec = '0' + sec;
+  }
+  return min + ':' + sec;
+}
+
+function getDefaultState() {
+  return {
+    sessionLength: 25,
+    breakLength: 5,
+    timerLabel: 'Session',
+    timeLeft: 25 * 60 * 1000,
+    countdown: false,
+    intervalID: null
+  };
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      sessionLength: 25,
-      breakLength: 5,
-      timerLabel: 'Session'
-    };
+    this.state = getDefaultState();
+    this.changeTimeLength = this.changeTimeLength.bind(this);
+    this.startCountdown = this.startCountdown.bind(this);
+    this.stopCountdown = this.stopCountdown.bind(this);
+    this.toggleCountdown = this.toggleCountdown.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  changeTimeLength(timerType, value) {
+    this.setState({ countdown: false });
+    let timeLength = timerType === 'Session' ? this.state.sessionLength : this.state.breakLength;
+    let newLength = timeLength + value;
+    if (newLength > 0 && newLength < 61) {
+      this.setState(timerType === 'Session' ? { sessionLength: newLength } : { breakLength: newLength });
+      if (timerType === this.state.timerLabel) {
+        this.setState({ timeLeft: newLength * 60 * 1000 });
+      }
+    }
+  }
+
+  startCountdown() {
+    const intervalID = setInterval(() => {
+      let timeLeft = this.state.timeLeft - 1000;
+      if (timeLeft < 0) {
+        if (this.state.timerLabel === 'Session') {
+          this.setState({ timerLabel: 'Break' });
+          timeLeft = this.state.breakLength * 60 * 1000;
+        } else {
+          this.setState({ timerLabel: 'Session' });
+          timeLeft = this.state.sessionLength * 60 * 1000;
+        }
+      }
+      if (timeLeft === 0) {
+        document.getElementById('beep').play();
+      }
+      this.setState({ timeLeft: timeLeft });
+    }, 1000);
+    this.setState({ countdown: true, intervalID });
+  }
+
+  stopCountdown() {
+    clearInterval(this.state.intervalID);
+    this.setState({ countdown: false, intervalID: null });
+  }
+
+  toggleCountdown() {
+    this.state.countdown ? this.stopCountdown() : this.startCountdown();
+  }
+
+  handleStop() {
+    this.stopCountdown();
+    this.setState({ timeLeft: this.state.sessionLength * 60 * 1000 });
+  }
+
+  handleReset() {
+    this.stopCountdown();
+    this.setState(getDefaultState());
+    document.getElementById('beep').pause();
+    document.getElementById('beep').currentTime = 0;
   }
 
   render() {
@@ -16,23 +97,41 @@ class App extends React.Component {
       <div id="clock">
         <div id="session-setting">
           <p id="session-label">Session Length</p>
-          <button id="session-decrement">down</button>
-          <div id="session-length">{this.state.sessionLength}</div>
-          <button id="session-increment">up</button>
+          <button id="session-decrement" onClick={() => this.changeTimeLength('Session', -1)}>
+            down
+          </button>
+          <div id="session-length-wrapper">
+            <span id="session-length">{this.state.sessionLength}</span> min
+          </div>
+          <button id="session-increment" onClick={() => this.changeTimeLength('Session', 1)}>
+            up
+          </button>
         </div>
         <div id="break-setting">
-          <p id="break-label">break Length</p>
-          <button id="break-decrement">down</button>
-          <div id="break-length">{this.state.breakLength}</div>
-          <button id="break-increment">up</button>
+          <p id="break-label">Break Length</p>
+          <button id="break-decrement" onClick={() => this.changeTimeLength('Break', -1)}>
+            down
+          </button>
+          <div id="break-length-wrapper">
+            <span id="break-length">{this.state.breakLength}</span> min
+          </div>
+          <button id="break-increment" onClick={() => this.changeTimeLength('Break', 1)}>
+            up
+          </button>
         </div>
         <div id="timer">
           <p id="timer-label">{this.state.timerLabel}</p>
-          <div id="time-left">25:00 {/* TODO */}</div>
-          <button id="start_stop">start/stop</button>
-          <button id="restart">restart</button>
-          <button id="reset">reset</button>
-          <audio id="beep"></audio>
+          <div id="time-left">{formatTime(this.state.timeLeft)}</div>
+          <button id="start_stop" onClick={this.toggleCountdown}>
+            start/stop
+          </button>
+          <button id="stop" onClick={this.handleStop}>
+            stop
+          </button>
+          <button id="reset" onClick={this.handleReset}>
+            reset
+          </button>
+          <audio id="beep" src="/birds-singing.wav"></audio>
         </div>
       </div>
     );
